9ae453381366578664b5efd6c69c923599a9d0ad
Bryan Yan <bryanyan.tx@gmail.com>
2023-07-05 12:20:11 +0800

    added home and login/logout urls

--- prediction_app/era_predictions/views.py
+++ prediction_app/era_predictions/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
 
-# Create your views here.
+def home(request):
+    return render(request, 'era_predictions/home.html')
+
+def login_view(request):
+    if request.method == 'POST':
+        # Handle login form submission
+        # Authenticate user and log in
+        pass
+    else:
+        # Render the login form
+        return render(request, 'era_predictions/login.html')
+
+@login_required
+def logout_view(request):
+    # Log out the user
+    logout(request)
+    return redirect('home')
