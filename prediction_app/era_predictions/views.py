7e9d4f774c2576859f303d1ce73a62924e5ff472
Bryan Yan <bryanyan.tx@gmail.com>
2023-07-06 09:33:25 +0800

    redirect to login page

--- prediction_app/era_predictions/views.py
+++ prediction_app/era_predictions/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
 
+@login_required(login_url='login')
 def home(request):
     return render(request, 'era_predictions/home.html')
 
