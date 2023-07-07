2f235374ee8f30bea3a47448605683268d8ba87b
Bryan Yan <bryanyan.tx@gmail.com>
2023-07-07 15:08:44 +0800

    show list of registered users

--- prediction_app/era_predictions/views.py
+++ prediction_app/era_predictions/views.py
@@ -2,10 +2,12 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
 
 @login_required(login_url='login')
 def home(request):
-    return render(request, 'era_predictions/home.html')
+    users = User.objects.all()
+    return render(request, 'era_predictions/home.html', {'users': users})
 
 def login_view(request):
     if request.method == 'POST':
